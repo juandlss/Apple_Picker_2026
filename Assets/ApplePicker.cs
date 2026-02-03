3f1abcf31776724c30ff45514a9a7089b909210e
KeyangYu <kyu009@fiu.edu>
2026-02-03 16:43:08 -0600

    Done Demo

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -10,31 +11,47 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
     
     void Start()
     {
         // Use For loop to create numBaskets of baskets,
         // Vector3 position initialized as (0, 0, 0), then update Y axis with BottomY and SpacingY
+        basketList = new List<GameObject>();
         for (int i = 0; i < numBaskets; i++)
         {
             GameObject tBasketGo = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGo.transform.position = pos;
+            basketList.Add(tBasketGo);
         }
     }
     
     // AppleMissed()
-    // Destroy all of the falling apples
-    
-    // Destroy one of the Baskets
-    // Get the index of the last Basket in basketList
-    
-    // Get a reference to that Basket GameObject
-    
-    // Remove the Basket from the list and destroy the GameObject
-    
-    
-    // If there are no Baskets left, restart the game
+    public void AppleMissed()
+    {
+        // Destroy all of the falling apples
+        GameObject[] appleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject tApple in appleArray)
+        {
+            Destroy(tApple);
+        }
+
+        // Destroy one of the Baskets
+        int basketIndex = basketList.Count - 1;
+        // Get the index of the last Basket in basketList
+        GameObject basketGo = basketList[basketIndex];
+        // Get a reference to that Basket GameObject
+        // Remove the Basket from the list and destroy the GameObject
+        basketList.RemoveAt(basketIndex);
+        Destroy(basketGo);
+        
+        // If there are no Baskets left, restart the game
+        if (basketList.Count == 0)
+        {
+            SceneManager.LoadScene("_Scene_0");
+        }
+    }
 
 }
